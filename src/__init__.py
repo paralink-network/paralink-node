3ad660596cb65a5bf6d53eb5e9a25eed74fe359a
jbargu <jure.grabnar@gmail.com>
2020-12-22 13:36:15 +0100

    Add execute_ipfs endpoint

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,14 @@
 import json
 
+from requests.exceptions import ReadTimeout
 from sanic import Sanic
 from sanic.log import logger
 from sanic_jsonrpc import SanicJsonrpc
 
+import ipfshttpclient
+
 from src.pql.parser import parse_and_execute
+from src.pql.exceptions import PqlDecodingError
 
 
 def create_app(args) -> Sanic:
@@ -15,6 +19,26 @@ def create_app(args) -> Sanic:
     async def execute_pql(pql_json: str) -> str:
         logger.info(f"Execute PQL {pql_json} request.")
         req = json.loads(pql_json)
+
+        res = await parse_and_execute(req)
+        logger.info(f"Obtained result {res}")
+
+        return res
+
+    @jsonrpc
+    async def execute_ipfs(ipfs_address: str, ipfs_hash: str) -> str:
+        logger.info(f"Execute IPFS PQL {ipfs_address}/{ipfs_hash} request.")
+
+        try:
+            # Connect to the IPFS API server
+            ipfs = ipfshttpclient.connect(ipfs_address)
+
+            # Fetch the JSON from the hash
+            req = ipfs.get_json(ipfs_hash, timeout=args.timeout)
+        except ipfshttpclient.expections.DecodingError:
+            raise PqlDecodingError("object decoding error, expecting JSON format.")
+        except ReadTimeout:
+            raise PqlDecodingError("IPFS timed out before retrieving the file.")
 
         res = await parse_and_execute(req)
         logger.info(f"Obtained result {res}")
--- src/pql/exceptions.py
+++ src/pql/exceptions.py
@@ -4,6 +4,17 @@ from fashionable.unset import UNSET
 from sanic_jsonrpc.errors import Error
 
 
+class PqlDecodingError(Error):
+    """PqlDecodingError is triggered when given PQL definition cannot be decoded (this includes incorrect format as well as
+    empty PQL definition - in case it is not found).
+    """
+
+    def __init__(self, message: str):
+        self.code = -32001
+        self.message = message
+        self.data = UNSET
+
+
 class NoInputValue(Error):
     """NoInputValue is triggered when there is not value from the previous step to act
     upon.
