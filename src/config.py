90b3c6b3f0c409c9c02e33f35ea0bf3076057838
jbargu <jure.grabnar@gmail.com>
2020-12-23 12:44:54 +0100

    Update config with IPFS local server

--- src/config.py
+++ src/config.py
@@ -6,15 +6,44 @@ from src.pql.handlers.rest_api_handler import RestApiHandler
 # Load the main configuration file
 load_dotenv(".env")
 
-# Ethereum
-ETHERSCAN_KEY = getenv("ETHERSCAN_KEY")
-INFURA_KEY = getenv("INFURA_KEY")
-GETH_HTTP_RPC = getenv("GETH_HTTP_RPC", "http://localhost:8745")
-
 
 class Config:
     DEBUG = False
     TESTING = False
+
+    # Ethereum
+    ETHERSCAN_KEY = getenv("ETHERSCAN_KEY")
+    INFURA_KEY = getenv("INFURA_KEY")
+    GETH_HTTP_RPC = getenv("GETH_HTTP_RPC", "http://localhost:8745")
+    IPFS_API_SERVER_ADDRESS = getenv("IPFS_API_SERVER_ADDRESS")
+
+    TEMPLATE_PQL_DEFINITION = {
+        "name": "My job name",
+        "psql_version": "0.1",
+        "sources": [
+            {
+                "name": "Pipeline 1 name",
+                "pipeline": [
+                    {
+                        "step": "extract",
+                        "method": "http.get",
+                        "uri": "https://my-crypto-api.com/tickers",
+                    },
+                ],
+            },
+            {
+                "name": "Pipeline 2 name",
+                "pipeline": [
+                    {
+                        "step": "extract",
+                        "method": "http.get",
+                        "uri": "https://my-crypto-api.com/tickers",
+                    },
+                ],
+            },
+        ],
+        "aggregate": {"method": "mean"},
+    }
 
     # User defined custom functions
     PQL_CUSTOM_METHODS = {
