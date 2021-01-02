41a369b49aa07cbce005c31b98e8587fda25a083
jbargu <jure.grabnar@gmail.com>
2021-01-02 15:50:53 +0100

    Add missing changes config/w3

--- src/cli/node.py
+++ src/cli/node.py
@@ -1,14 +1,14 @@
-import argparse
-import textwrap
 from docopt import docopt
 
 from src import create_app
+from src.config import config
+from src.network import accounts
 
 __doc__ = """
 Usage: paralink-node node <command> [<arguments> ...] [options]
 
 Commands:
-   start [--host]           Start the node.
+   start                    Start the node.
 
 Options:
    --host <host>            Specify host to run on [default: 127.0.0.1]
@@ -23,8 +23,10 @@ def main():
     args = docopt(__doc__)
 
     if args["<command>"] == "start":
+        accounts.load()
+
         app = create_app(args)
-        app.run(host=args["--host"], port=args["--port"], debug=True)
+        app.run(host=args["--host"], port=args["--port"], debug=config.DEBUG)
 
 
 if __name__ == "__main__":
---
+++ src/network/web3.py
@@ -0,0 +1,6 @@
+from dotenv import load_dotenv
+from web3 import Web3
+
+load_dotenv(".env")
+
+w3 = Web3()
