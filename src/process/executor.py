3e7a86d714ceba5f03a765f434f2744550582b1e
jbargu <jure.grabnar@gmail.com>
2021-01-04 21:07:41 +0100

    Execute queue can now use write_to_eth_chain function

--- src/process/executor.py
+++ src/process/executor.py
@@ -10,6 +10,7 @@ from src.network.web3 import w3
 
 from src.pql.parser import parse_and_execute
 from src.utils.ipfs import bytes32_to_ipfs
+from src.network import accounts
 
 logger = get_task_logger(__name__)
 
@@ -22,7 +23,6 @@ def handle_request_event(self, event) -> None:
     Args:
         event: Solidity Request event
     """
-    print(event)
     args = event["args"]
 
     ipfs_hash = bytes32_to_ipfs(args["ipfsHash"])
@@ -37,8 +37,7 @@ def handle_request_event(self, event) -> None:
         res = asyncio.run(parse_and_execute(req))
         logger.info(f"Obtained result {res}")
 
-        # TODO: use callback
-        print(f"Writing {res} to callback")
+        write_to_eth_chain(event, res)
     except Exception as e:
         if datetime.now() < expiration:
             self.retry(exc=e)
@@ -47,3 +46,32 @@ def handle_request_event(self, event) -> None:
                 f"Request {args['requestId']} from {args['requester']} expired due to {str(type(e))}: {e.args[0]}"
             )
             return
+
+
+def write_to_eth_chain(event, res):
+    """It writes `res` (result of the PQL definition) to the location specified in the `Request` event.
+
+    Args:
+        event: a Request event from ETH chain.
+        res: already executed PQL definition
+    """
+    logger.info(f"Writing {res} to ETH chain")
+
+    args = event["args"]
+    contract = w3.eth.contract(abi=config.ORACLE_CONTRACT_ABI, address=event["address"])
+
+    tx = contract.functions.fulfillRequest(
+        args["requestId"],
+        args["callbackAddress"],
+        args["callbackFunctionId"],
+        args["expiration"],
+        w3.toBytes(int(res)).rjust(32, b"\0"),
+    ).buildTransaction()
+
+    tx.update({"nonce": w3.eth.getTransactionCount(accounts.ethkey.address)})
+
+    signed_tx = w3.eth.account.signTransaction(tx, accounts.ethkey.privateKey)
+    tx_hash = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
+    tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+
+    logger.info(tx_receipt)
