5429bbef1fd756e021bcfcd23bf646cc14aa16c9
jbargu <jure.grabnar@gmail.com>
2021-01-28 18:02:58 +0100

    Add collect/execute logic for substrate and update EVM logic

--- src/process/collector.py
+++ src/process/collector.py
@@ -1,31 +1,98 @@
 import time
 
+from celery.utils.log import get_task_logger
+
 from src.config import config
 from src.models import ContractOracle, db
-from src.network.web3 import w3
+from src.network.chains import Chains
+from src.network.evm_chain import EvmChain
+from src.network.substrate_chain import SubstrateChain
 from src.process import processor
-from src.process.executor import handle_request_event
+from src.process.executor import (
+    handle_evm_request_event,
+    handle_substrate_request_event,
+)
+
+logger = get_task_logger(__name__)
 
 
-async def start_collecting():
-    """Initiates collecting tasks for addresses specified in the PostgreSQL DB."""
-    for contract_oracle in await ContractOracle.query.gino.all():
-        listen_for_request_events.delay(contract_oracle.id, 2)
+async def start_collecting(chains: Chains) -> None:
+    """Initiates collecting tasks for addresses specified in the `chains` object."""
+    for chain_name, chain in chains.evm.items():
+        listen_for_evm_events.delay(chain)
+
+    for chain_name, chain in chains.substrate.items():
+        listen_for_substrate_events.delay(chain)
 
 
 @processor.task
-def listen_for_request_events(address: str, poll_interval: int) -> None:
-    """listen_for_request_events takes the given contract oracle address and
+def listen_for_evm_events(evm_chain: EvmChain, poll_interval=2) -> None:
+    """listen_for_request_events takes the given contract
+    oracle addresses and
     listens for any Request events.
 
     Args:
-        address: contract oracle address.
+        evm_chain: EvmChain to listen to events.
         poll_interval: time between the checks.
     """
-    contract = w3.eth.contract(abi=config.ORACLE_CONTRACT_ABI, address=address)
-    event_filter = contract.events.Request.createFilter(fromBlock="latest")
+    w3 = evm_chain.get_connection()
+
+    event_filters = []
+    for contract_address in evm_chain.tracked_contracts:
+        contract = w3.eth.contract(
+            abi=evm_chain.oracle_metadata, address=contract_address
+        )
+        filt = contract.events.Request.createFilter(fromBlock="latest")
+
+        logger.info(f"Creating filter {filt} for address {contract_address}.")
+        event_filters.append(filt)
 
     while True:
-        for event in event_filter.get_new_entries():
-            handle_request_event.delay(event)
+        for event_filter in event_filters:
+            for event in event_filter.get_new_entries():
+                logger.info(f"EVM Request found: {event}")
+
+                handle_evm_request_event.delay(evm_chain, event)
+
         time.sleep(poll_interval)
+
+
+@processor.task
+def listen_for_substrate_events(
+    substrate_chain: SubstrateChain, poll_interval=2
+) -> None:
+    """listen_for_substrate_events takes the given substrate chain with `tracked_contracts`
+    addresses and watches for any `Request` events on the chain.
+
+    Args:
+        substrate_chain: SubstrateChain to listen to events.
+        poll_interval: time between the checks.
+    """
+    substrate = substrate_chain.get_connection()
+
+    finalised_block_nr = substrate.get_block_number(
+        substrate.get_chain_finalised_head()
+    )
+
+    # Iterate over every block
+    while True:
+        block_hash = substrate.get_block_hash(finalised_block_nr)
+
+        # If the block exists, check for any events, otherwise go back to sleep
+        if block_hash:
+            events = substrate_chain.get_block_events(block_hash)
+
+            for event, decoded_event in events:
+                logger.info(
+                    f"Found event {event} | {decoded_event}, block_nr {finalised_block_nr}, "
+                )
+                # TODO: add delay
+                handle_substrate_request_event(
+                    substrate_chain,
+                    decoded_event,
+                    {"params": event.params},
+                )
+
+            finalised_block_nr += 1
+        else:
+            time.sleep(poll_interval)
