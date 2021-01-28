bb6feee5e08eeed499e3f27da422765fd0a4fd76
jbargu <jure.grabnar@gmail.com>
2021-01-28 17:52:55 +0100

    Add Chain object abstraction

---
+++ src/network/chain.py
@@ -0,0 +1,26 @@
+class Chain:
+    """Blockchain abstraction for different chains."""
+
+    def __init__(
+        self,
+        name: str,
+        url: str,
+        credentials: dict = {},
+        active: bool = True,
+        tracked_contracts: list = [],
+    ):
+        """Inits Chain object.
+
+        Args:
+            name (str): Name of the chain, should be unique.
+            url (str): URL of the chain.
+            credentials (dict): a dict of credentials for the chain, differs from chain to
+                                chain.
+            active (bool): whether the chain is active or inactive
+            tracked_contracts (list): list of contracts to listen for events
+        """
+        self.name = name
+        self.url = url
+        self.credentials = credentials
+        self.tracked_contracts = tracked_contracts
+        self.active = active
